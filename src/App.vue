25ae45b4e01f81e95f8648254e552f12c3c9a91a
Brendan Weibel <bweibel@microsoft.com>
2021-04-24 16:15:41 -0700

    remove stuff from components

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,10 @@ export default defineComponent({
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ff00ff;
   margin-top: 60px;
 }
 </style>
