5b5135137a3d3c9ba8fd74ba53fd83c8c81fb4d5
Brendan Weibel <bweibel@microsoft.com>
2021-04-24 16:12:35 -0700

    Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li>Hi</li>
+    <li>Hi</li>
+    <li>Hi</li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+});
+</script>
+
+<style scoped>
+li {
+  color: #00FF00;
+}
+</style>
