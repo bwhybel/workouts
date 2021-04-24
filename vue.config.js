0accf58a4934bea9764d62ef0a14c1ea738f2635
Brendan Weibel <bweibel@microsoft.com>
2021-04-24 14:58:56 -0700

    Remove unnecesasry

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-	? '/workouts/'
-	: '/'
-}
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/workouts/'
+    : '/',
+};
